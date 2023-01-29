6df0ce00fbc5bbfbe05a7a8d27f36ffeb716d0aa
VaidasiganLorena <92209059+VaidasiganLorena@users.noreply.github.com>
2023-01-29 15:36:37 +0200

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 import './App.css'
+import Homepage from './Homepage'
 
 function App() {
-  return <div className="App">hello word !!</div>
+  return (<><div className="App">hello word !!</div><Homepage/></>)
 }
 
 export default App
